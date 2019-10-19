05e95fe3819c5ebfffb2c999d44134b858dc378d
tjakubo2 <tofix112@gmail.com>
2019-10-19 20:07:39 +0200

    Changed Timer based wait to Wait.condition, slight refactor

--- TTSDiceCalculator.lua
+++ TTSDiceCalculator.lua
@@ -1,4 +1,3 @@
-DELAY_TIME = 0.1 --Increasing this number should increase performance but make the calculator slower
 rolls = {}
 function onObjectRandomize(object, player_color)
     local steam_name = Player[player_color].steam_name
@@ -11,28 +10,50 @@ function onObjectRandomize(object, player_color)
     end
 end
 
+local function group_dice(dice)
+    counts = {}
+    counts[20] = {}
+    total = 0
+    for k, v in pairs(dice) do
+        diceType = #v.getRotationValues()
+        value = v.getValue() or 0
+        counts[diceType] = counts[diceType] or {}
+        table.insert(counts[diceType], value)
+        if diceType != 20 then
+            total = total + value
+        end
+    end
+    return counts, total
+end
+
+local function finishMessage(steam_name, dice)
+    counts, total = group_dice(dice)
+    message = steam_name .. " has rolled "
+    if #counts[20] > 0 then
+        message = message .. #counts[20] .. "d20 (" .. table.concat(counts[20], ",") .. ") "
+        if total > 0 then
+            message = message .. "and "
+        end
+    end
+    for k, v in pairs(counts) do
+        if k != 20 and k != 0 then
+            message = message .. #v .. "d" .. k .. "+"
+        end
+    end
+    if total > 0 then
+        message = message:sub( 1, string.len(message) - 1) .. "=" .. total
+    end
+    return message
+end
+
 function newPlayerRoll(steam_name)
     local self = {
         dice = {},
         steam_name = steam_name,
+        wait_id = nil
     }
 
-    local still_rolling = function()
-        Timer.destroy(self.steam_name .. ":roll_timer")
-        Timer.create({
-            identifier = self.steam_name .. ":roll_timer",
-            function_name = "finishRoll",
-            parameters = { self.steam_name },
-            delay = DELAY_TIME, --in seconds
-        })
-    end
-
-    local add = function(object)
-        still_rolling()
-        self.dice[object.getGUID()] = object
-    end
-
-    local really_finished = function()
+    local function wait_function()
         for k, v in pairs(self.dice) do
             if not v.resting then
                 return false
@@ -41,55 +62,37 @@ function newPlayerRoll(steam_name)
         return true
     end
 
-    local groupDice = function()
-        counts = {}
-        counts[20] = {}
-        total = 0
-        for k, v in pairs(self.dice) do
-            diceType = #v.getRotationValues()
-            value = v.getValue() or 0
-            counts[diceType] = counts[diceType] or {}
-            table.insert(counts[diceType], value)
-            if diceType != 20 then
-                total = total + value
-            end
-        end
-        return counts, total
+    local function finish()
+        message = finishMessage(self.steam_name, self.dice)
+        broadcastToAll(message, {0,255,0})
+        self.dice = {}
+        Wait.stop(self.wait_id)
+        self.wait_id = nil
     end
 
-    local finishMessage = function()
-        counts, total = groupDice()
-        message = self.steam_name .. " has rolled "
-        if #counts[20] > 0 then
-            message = message .. #counts[20] .. "d20 (" .. table.concat(counts[20], ",") .. ") "
-            if total > 0 then
-                message = message .. "and "
-            end
-        end
-        for k, v in pairs(counts) do
-            if k != 20 and k != 0 then
-                message = message .. #v .. "d" .. k .. "+"
-            end
-        end
-        if total > 0 then
-            message = message:sub( 1, string.len(message) - 1) .. "=" .. total
-        end
-        return message
+    local function start_wait()
+        self.wait_id = Wait.condition(finish, wait_function)
     end
 
-    local finish = function()
-        if not really_finished() then
-            still_rolling()
-        else
-            message = finishMessage()
-            broadcastToAll(message, {0,255,0})
-            self.dice = {} -- can optimize this if RAM becomes an issue
+    local function add(object)
+        self.dice[object.getGUID()] = object
+        if not self.wait_id then
+            start_wait()
         end
+    end
+
+    local function stop()
+        if self.wait_id then
+            Wait.stop(self.wait_id)
+            return true
+        end
+        return false
     end
 
     return {
         add = add,
         start_roll = add,
+        stop_roll = stop,
         finish = finish,
     }
 end
